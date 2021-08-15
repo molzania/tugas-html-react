0f24f62c6ed11bc0ebc569d4497e60847738eb70
molzania <molzania_1507@yahoo.co.id>
2021-08-15 12:32:54 +0700

    commit on 'main'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import molzania from '../img/molzania.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<body>
+<header> 
+        <h1 className="title">PORTOFOLIO</h1>
+        <div className="navbar">
+            <a className="menu" href="#Home">Home</a>
+            <a className="menu" href="#About">Blog</a>
+            <a className="menu" href="#Work">Work</a>
+            <a className="menu" href="#Contact">Contact</a>
+        </div>
+    </header>
+    <div className="about" alt="about">
+            <img src={molzania} className="profil" alt="molzania" />
+            <h2 className="h2">Hi, Saya Molzania!</h2>
+            <p className="text">Saya memiliki hobi menulis sejak kecil. Saya bekerja sebagai konten kreator dan desainer grafis.
+            Saya memiliki blog, youtube dan berbagai platform sosial media. </p>
+            <p className="text">Nama asli saya Firsty Ukhti Molyndi. Saat ini saya tinggal di Palembang. Saya memulai blog sejak tahun 2009.</p>
+        </div>
+</body>
   );
 }
 
