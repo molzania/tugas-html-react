2e0ca43711118ef6ec8bf86efe75d5e40cbce600
molzania <molzania_1507@yahoo.co.id>
2021-08-18 13:04:35 +0700

    commit on 'main'

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
 <header> 
         <h1 className="title">PORTOFOLIO</h1>
         <div className="navbar">
-            <a className="menu" href="#Home"><Link to="./Home">Home</Link></a>
+            <a className="menu"><Link to="./Home">Home</Link></a>
             <a className="menu" href="https://www.molzania.com">Blog</a>
-            <a className="menu" href="#Work"><Link to="">Work</Link></a>
-           <a className="menu" href="#Contact"><Link to="./Contact">Contact</Link></a>
+            <a className="menu"><Link to="">Work</Link></a>
+           <a className="menu"><Link to="./Contact">Contact</Link></a>
         </div>
     </header>
  <Main/>
