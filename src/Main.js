1a713b452033265ca7d242f40be81b69dbd80dc5
molzania <molzania_1507@yahoo.co.id>
2021-08-19 10:18:22 +0700

    commit on 'main'

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
@@ -6,8 +6,10 @@ import Contact from './Contact';
 import Work from './Work';
 
 const Main = () => {
+  
   return (
     <Switch> {/* The Switch decides which component to show based on the current URL.*/}
+      <Route exact path='/' component={Home}></Route>
       <Route exact path='/Home' component={Home}></Route>
       <Route exact path='/Contact' component={Contact}></Route>
       <Route exact path='/Work' component={Work}></Route>
